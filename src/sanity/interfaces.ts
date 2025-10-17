ae0999414d8fe38a480adff05f19b7d7b5e90e7c
aar-woo <awooyes@gmail.com>
2025-10-16 19:04:38 -1000

    Matching typescript interfaces for sanity schemas.

--- src/sanity/interfaces.ts
+++ src/sanity/interfaces.ts
@@ -9,14 +9,78 @@ export interface SanityServiceCard {
     };
     alt: string;
   };
-  primaryCta?: {
-    label: string;
-    href: string;
-    variant?: "default" | "outline" | "oval";
+  buttonLabel?: string;
+  buttonLink?: string;
+}
+
+export interface SanityContentBlock {
+  _id: string;
+  title: string;
+  description: string;
+  image: {
+    asset: {
+      _ref: string;
+      _type: string;
+    };
+    alt: string;
   };
-  secondaryCta?: {
-    label: string;
-    href: string;
-    variant?: "default" | "outline" | "oval";
+  buttonLabel?: string;
+  buttonLink?: string;
+  order: number;
+  type: "about" | "parallax";
+}
+
+export interface SanityHero {
+  _id: string;
+  title: string;
+  subtitle: string;
+  backgroundImage: {
+    asset: {
+      _ref: string;
+      _type: string;
+    };
   };
+  primaryButtonText: string;
+  primaryButtonLink: string;
+  secondaryButtonText?: string;
+  secondaryButtonLink?: string;
+}
+
+export interface SanityGalleryImage {
+  image: {
+    asset: {
+      _ref: string;
+      _type: string;
+    };
+  };
+  alt: string;
+  caption?: string;
+}
+
+export interface SanityGallery {
+  _id: string;
+  title?: string;
+  images: SanityGalleryImage[];
+}
+
+export interface SanityBusinessInfo {
+  address: string;
+  phone: string;
+  email: string;
+  hours: string;
+}
+
+export interface SanitySocialLinks {
+  facebook?: string;
+  instagram?: string;
+  twitter?: string;
+}
+
+export interface SanitySiteSettings {
+  _id: string;
+  brandName: string;
+  tagline?: string;
+  businessInfo?: SanityBusinessInfo;
+  socialLinks?: SanitySocialLinks;
+  showNewsletter: boolean;
 }
