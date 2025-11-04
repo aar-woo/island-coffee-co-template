d1548279ba2b095e6d376e1ee7b2843c69ebfea5
aar-woo <awooyes@gmail.com>
2025-11-04 11:16:03 -1000

    Added get directions button on top of map that sets current position for now.

--- src/components/ui/Map/Map.tsx
+++ src/components/ui/Map/Map.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { Button } from "../base/button";
 
 const containerStyle = {
   width: "100%",
@@ -11,13 +12,63 @@ const center = {
   lng: -157.8583,
 };
 
+interface Position {
+  lat: number;
+  lng: number;
+}
+
+async function getCurrentPosition(): Promise<Position> {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        resolve({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      },
+      (error) => {
+        reject(error);
+      }
+    );
+  });
+}
+
 function Map() {
+  const [currentPosition, setCurrentPosition] = useState<Position | null>(null);
+
+  const handleGetCurrentPosition = async () => {
+    const position = await getCurrentPosition();
+    setCurrentPosition(position);
+  };
   return (
-    <LoadScript googleMapsApiKey={process.env.GOOGLE_MAPS_EMBED_API_KEY || ""}>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={14}>
-        <Marker position={center} />
-      </GoogleMap>
-    </LoadScript>
+    <div style={{ position: "relative", width: "100%", height: "100%" }}>
+      <LoadScript
+        googleMapsApiKey={process.env.GOOGLE_MAPS_EMBED_API_KEY || ""}
+      >
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={14}>
+          <Marker position={center} />
+        </GoogleMap>
+      </LoadScript>
+
+      <Button
+        type="button"
+        style={{
+          position: "absolute",
+          top: "10px",
+          right: "60px",
+          padding: "18px 20px",
+          backgroundColor: "white",
+          color: "black",
+          border: "2px solid #ccc",
+          borderRadius: "4px",
+          cursor: "pointer",
+          boxShadow: "0 2px 6px rgba(0,0,0,0.3)",
+        }}
+        onClick={handleGetCurrentPosition}
+      >
+        Get Directions
+      </Button>
+    </div>
   );
 }
 
